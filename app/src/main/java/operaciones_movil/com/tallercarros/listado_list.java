c930b3d1d759e4e63de9a7c029849c850ce19f67
Jaime <j.efonsecaguerrero@gmail.com>
2018-10-04 21:04:41 -0500

    Prueba

--- app/src/main/java/operaciones_movil/com/tallercarros/listado_list.java
+++ app/src/main/java/operaciones_movil/com/tallercarros/listado_list.java
@@ -4,23 +4,24 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+
 import java.util.ArrayList;
 
 public class listado_list extends AppCompatActivity {
-    private LinearLayout lista;
+    private ListView lista;
     private ArrayList<Carro> carros;
     private Context contexto;
     private adapter_carro adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_principal);
+        setContentView(R.layout.activity_listado_list_view);
 
-        lista = (LinearLayout) findViewById(R.id.list_personalizado);
+        lista = (ListView) findViewById(R.id.list_personalizado);
         contexto = this .getApplicationContext();
         carros = datos.obtenerCarros();
         adapter = new adapter_carro(contexto,carros);
         lista.setAdapter(adapter);
-
     }
 }
